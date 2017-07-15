5632a98e8571d5c9f062806ba34daec049e85ce8
Suveshtr <suvesh.trivedi@gmail.com>
2017-07-15 10:28:57 +0530

    Refactor to create Book component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import Book from './Book'
 class BookShelf extends React.Component {
   
   static propTypes = {
@@ -21,36 +21,7 @@ class BookShelf extends React.Component {
               <ol className="books-grid">
                 {books.map((book) =>
                   <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                          }}
-                        >
-                        </div>
-                        <div className="book-shelf-changer">
-                          <select 
-                            onChange={event => onChangeShelf(book, event.target.value)}
-                            defaultValue={book.shelf}
-                          >
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">
-                        {book.authors.map((author) =>
-                          <p key={author}>{author}</p>
-                        )}
-                      </div>
-                    </div>
+                    <Book book={book} shelfTitle={shelfTitle} onChangeShelf={onChangeShelf} />
                   </li>
                 )}
               </ol>
